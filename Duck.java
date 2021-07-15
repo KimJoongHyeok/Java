fdba9e90b65a99647ba16247cd825858352a56e7
KimJoongHyeok <83209006+KimJoongHyeok@users.noreply.github.com>
2021-07-15 22:05:47 +0900

    Java

---
+++ Duck.java
@@ -0,0 +1,13 @@
+
+
+public class Duck extends Bird{
+
+	@Override
+	public void sing() {
+		// TODO Auto-generated method stub
+		System.out.println("노래부르다.");
+		
+	}
+	
+
+}
---
+++ DuckExam.java
@@ -0,0 +1,10 @@
+
+public class DuckExam {
+
+	public static void main(String [] args) {
+		Duck dk = new Duck();
+		
+		dk.fly();
+		dk.sing();
+	}
+}
